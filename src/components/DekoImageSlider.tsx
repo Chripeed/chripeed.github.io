094943b41bff16285f5190e8460df6d4553f051c
Chripeed <christopher.peedoson@gmail.com>
2023-12-19 00:23:42 +0200

    fullscreen images

--- src/components/DekoImageSlider.tsx
+++ src/components/DekoImageSlider.tsx
@@ -56,11 +56,44 @@ function ImageSlider() {
     previewContainerRef.current.scrollLeft = scrollPosition;
   };
 
+  const [fullscreenImage, setFullscreenImage] = useState(null);
+
+  const openFullscreenImage = () => {
+    setFullscreenImage(slides[currentIndex].url);
+  };
+
+  const FullscreenModal = ({ image, onClose }) => {
+    if (!image) return null;
+
+    return (
+      <div className="fixed inset-0 bg-deep-black bg-opacity-90 flex items-center justify-center z-50">
+        {/* Overlay with semi-transparent black background */}
+        <div className="absolute inset-0 bg-black bg-opacity-75"></div>
+
+        {/* Image */}
+        <img
+          src={image}
+          alt="Fullscreen"
+          className="max-w-full max-h-full z-10"
+        />
+
+        {/* Close button */}
+        <button
+          onClick={onClose}
+          className="absolute top-4 right-4 text-pure-white text-4xl z-10"
+        >
+          ⨉
+        </button>
+      </div>
+    );
+  };
+
   return (
     <div className="h-[400px] md:h-[700px] md:max-w-[1200px] w-full m-auto py-2 px-4 relative group mt-8">
       <div
         style={{ backgroundImage: `url(${slides[currentIndex].url})` }}
         className="w-full h-full rounded-2xl bg-center bg-cover duration-500"
+        onClick={openFullscreenImage}
       ></div>
 
       {/* Left arrow */}
@@ -84,6 +117,10 @@ function ImageSlider() {
           />
         ))}
       </div>
+      <FullscreenModal
+        image={fullscreenImage}
+        onClose={() => setFullscreenImage(null)}
+      />
     </div>
   );
 }
