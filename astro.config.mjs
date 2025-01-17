c1def6b8da891ad662b92c823bfded3500e2826c
Chripeed <christopher.peedoson@gmail.com>
2025-01-17 16:52:04 +0200

    .

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,14 +2,13 @@
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
-import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://chripeed.github.io",
-  output: "static", // or 'static' if you only want certain pages on-demand
-  adapter: node({
-    mode: "standalone",
-  }),
+  vite: {
+    ssr: {
+      noExternal: ["react-icons"],
+    },
+  },
   integrations: [react(), tailwind()],
 });
